4c2aea0ccc611fb87f0400264b1ae9fd2617a510
Daniel López <daniel315.a@gmail.com>
2021-07-25 23:37:42 -0500

    Se crea el método de establecer un usuario en el localStorage

--- src/app/models/Utilidades.ts
+++ src/app/models/Utilidades.ts
@@ -1,4 +1,10 @@
+import { Usuario } from "./usuario";
+
 export class Utilidades {
+    
+    static establecerUsuario(usuario: Usuario): void {
+      localStorage.setItem('usuario', JSON.stringify(usuario));
+    }
     
     public static verificarLogin(): boolean {
         return !!localStorage.getItem('usr-decoraytransforma');
