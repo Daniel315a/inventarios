fa6df23fc2041c9fe1090f6b83410817277bb129
Daniel López <daniel315.a@gmail.com>
2021-08-04 18:04:51 -0500

    Se maqueta el componente de personas

--- src/app/components/personas/personas.component.ts
+++ src/app/components/personas/personas.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
-  styleUrls: ['./personas.component.css']
+  styleUrls: ['./personas.component.css'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
-export class PersonasComponent implements OnInit {
+export class PersonasComponent implements OnInit, AfterViewInit {
+
+  /**
+   * Propiedades del diseño
+   */
+
+  public altoDivTabla: number = 0;
+  @ViewChild('divTabla')
+  public divTabla: ElementRef;
+  @ViewChild('divForm')
+  public divForm: ElementRef;
+
+  public labels = {
+    buscar: 'Buscar...'
+  };
+
+  public columnas = {
+    numeroDocumento: 'Número de documento',
+    nombreCompleto: 'Nombre completo',
+    tipoPersona: 'Tipo'
+  }
 
   constructor() { }
-
+  
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.onResize();
+    });
+  }
+
+  public onResize(){
+    let altoDivForm:number = this.divForm.nativeElement.offsetHeight;
+    let altoDivTabla = window.innerHeight - altoDivForm - 110;
+    
+    if(altoDivTabla != this.altoDivTabla){
+      this.altoDivTabla = altoDivTabla;
+    }
   }
 
 }
