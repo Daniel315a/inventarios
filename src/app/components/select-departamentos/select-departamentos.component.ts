6fb107d5f677e52fbbe5bbc455ebee32cc1ad4b6
Daniel López <daniel315.a@gmail.com>
2021-08-04 18:57:41 -0500

    Se llena el select de departamentos

--- src/app/components/select-departamentos/select-departamentos.component.ts
+++ src/app/components/select-departamentos/select-departamentos.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Departamento } from 'src/app/models/departamento';
+import { DepartamentoService } from 'src/app/services/departamento.service';
 
 @Component({
   selector: 'app-select-departamentos',
   templateUrl: './select-departamentos.component.html',
-  styleUrls: ['./select-departamentos.component.css']
+  styleUrls: ['./select-departamentos.component.css'],
+  providers: [
+    DepartamentoService
+  ]
 })
 export class SelectDepartamentosComponent implements OnInit {
 
   public departamento: Departamento = new Departamento();
   public departamentos: Array<Departamento> = new Array<Departamento>();
 
-  constructor() { }
+  constructor(
+    private _departamentoService: DepartamentoService
+  ) { }
 
   ngOnInit(): void {
+    this.consultarDepartamentos();
+  }
+
+  private consultarDepartamentos(){
+    this._departamentoService.consultarTodos().subscribe(
+      result => {
+        if(result.resultado){
+          this.departamentos = result.datos;
+        }
+      }
+    );
   }
 
   public onDepartamentoSeleccionado(){
