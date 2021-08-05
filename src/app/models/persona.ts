2915c8689a442f043e4d50e4261e45c50d26364d
Daniel López <daniel315.a@gmail.com>
2021-08-04 21:43:20 -0500

    Se cambia el nombre de la variable tipoPersona por tipo en Persona

--- src/app/models/persona.ts
+++ src/app/models/persona.ts
@@ -8,7 +8,7 @@ export class Persona{
     constructor(
         public id: number = 0,
         public municipio: Municipio = new Municipio(),
-        public tipoPersona: TipoPersona = new TipoPersona(),
+        public tipo: TipoPersona = new TipoPersona(),
         public empresa: Empresa = new Empresa(),
         public tipoDocumento: TipoDocumento = new TipoDocumento(),
         public numeroDocumento: string = '',
