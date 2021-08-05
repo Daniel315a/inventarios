3661bf1a1c7d22dc0c501bf6731526cf17800d47
Daniel López <daniel315.a@gmail.com>
2021-08-04 21:38:33 -0500

    Se maqueta el componente de tipos de persona y se llena el select

--- src/app/components/select-tipos-persona/select-tipos-persona.component.ts
+++ src/app/components/select-tipos-persona/select-tipos-persona.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { TipoPersona } from 'src/app/models/tipo-persona';
+import { TipoPersonaService } from 'src/app/services/tipo-persona.service';
 
 @Component({
   selector: 'app-select-tipos-persona',
   templateUrl: './select-tipos-persona.component.html',
-  styleUrls: ['./select-tipos-persona.component.css']
+  styleUrls: ['./select-tipos-persona.component.css'],
+  providers: [
+    TipoPersonaService
+  ]
 })
 export class SelectTiposPersonaComponent implements OnInit {
 
-  constructor() { }
+  public tipoPersona: TipoPersona = new TipoPersona();
+  public tiposPersona: Array<TipoPersona> = new Array<TipoPersona>();
+
+  constructor(
+    private _tipoPersonaService: TipoPersonaService
+  ) { }
 
   ngOnInit(): void {
+    this.consultarTiposPersona();
+  }
+
+  public consultarTiposPersona(){
+    this._tipoPersonaService.consultarTodos().subscribe(
+      result => {
+        if(result.resultado){
+          this.tiposPersona = result.datos;
+        }
+      }
+    );
   }
 
 }
