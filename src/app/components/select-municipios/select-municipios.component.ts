2f88a8d3f9bb20351f5f15b6c7afa42aeac8ced1
Daniel López <daniel315.a@gmail.com>
2021-08-04 19:06:32 -0500

    Se maqueta el componente select municipios

--- src/app/components/select-municipios/select-municipios.component.ts
+++ src/app/components/select-municipios/select-municipios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Municipio } from 'src/app/models/municipio';
 
 @Component({
   selector: 'app-select-municipios',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectMunicipiosComponent implements OnInit {
 
+  public municipio: Municipio = new Municipio();
+  public municipios: Array<Municipio> = new Array<Municipio>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public onMunicipioSeleccionado(){
+
   }
 
 }
