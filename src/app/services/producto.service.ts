c88f6ae11e124345519e3626d96e8ec03bb90a92
Daniel López <daniel315.a@gmail.com>
2021-08-06 00:06:27 -0500

    Se crean los métodos de crear y actualizar en el servicio de productos

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
+import { Producto } from "../models/producto";
 import { Utilidades } from "../models/utilidades";
 
 @Injectable()
@@ -39,6 +40,35 @@ export class ProductoService {
         set('token', usuario.token).
         set('solicitud', 'deshabilitar').
         set('id', id.toString());
+
+        return this._http.post(this.url, parametros);
+    }
+
+    public crear(producto: Producto): Observable<any>{
+        const usuario = Utilidades.obtenerUsuario();
+        const parametros: HttpParams = new HttpParams().
+        set('token', usuario.token).
+        set('solicitud', 'crear').
+        set('id_empresa', usuario.empresa.id).
+        set('referencia', producto.referencia).
+        set('detalle', producto.detalle).
+        set('cantidad_interna', producto.cantidadInterna.toString()).
+        set('cantidad_disponible', producto.cantidadDisponible.toString()).
+        set('precio', producto.precio.toString());
+
+        return this._http.post(this.url, parametros);
+    }
+
+    public actualizar(producto: Producto): Observable<any>{
+        const usuario = Utilidades.obtenerUsuario();
+        const parametros: HttpParams = new HttpParams().
+        set('token', usuario.token).
+        set('solicitud', 'actualizar').
+        set('id', producto.id.toString()).
+        set('detalle', producto.detalle).
+        set('cantidad_interna', producto.cantidadInterna.toString()).
+        set('cantidad_disponible', producto.cantidadDisponible.toString()).
+        set('precio', producto.precio.toString());
 
         return this._http.post(this.url, parametros);
     }
