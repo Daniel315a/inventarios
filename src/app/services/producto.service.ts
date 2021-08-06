b673aeb3260ba1b6dfcfaf93914739d7df8d7f70
Daniel López <daniel315.a@gmail.com>
2021-08-05 23:16:36 -0500

    Se crea el servicio de productos y el método de consultar por empresa

---
+++ src/app/services/producto.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Utilidades } from "../models/utilidades";
+
+export class ProductoService {
+
+    private url: string = environment.url + "Producto";
+
+    constructor(
+        private _http: HttpClient
+    ){}
+
+    public consultarPorEmpresa(): Observable<any>{
+        const usuario = Utilidades.obtenerUsuario();
+        const parametros: HttpParams = new HttpParams().
+        set('token', usuario.token).
+        set('solicitud', 'consultar_de_empresa').
+        set('id_empresa', usuario.empresa.id);
+
+        return this._http.get(this.url, {params: parametros});
+    }
+
+}
