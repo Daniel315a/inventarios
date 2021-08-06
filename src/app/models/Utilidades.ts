cc2ed99540c09f17cfceb35243e300d2af8042a8
Daniel López <daniel315.a@gmail.com>
2021-08-05 23:55:30 -0500

    Se agregan los diálogos a utilidades

--- src/app/models/Utilidades.ts
+++ src/app/models/Utilidades.ts
@@ -1,3 +1,4 @@
+import Swal from "sweetalert2";
 import { Usuario } from "./usuario";
 
 export class Utilidades {
@@ -21,5 +22,76 @@ export class Utilidades {
     public static desencriptar(cadena: string): string {
       return '';
     }
+
+    public static dialogSuccess(mensaje: string){
+      Swal.fire({
+          text: mensaje,
+          icon: 'success'
+      });
+  }
+  
+  public static dialogPregunta(titulo: string, mensaje: string, mensajeBtnEliminar: string): Promise<any>{
+      return Swal.fire({
+          title: titulo,
+          text: mensaje,
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#004570',
+          cancelButtonColor: '#BF0A19',
+          confirmButtonText: mensajeBtnEliminar,
+          cancelButtonText: 'Cancelar'
+        });
+  }
+
+  public static dialogErrorHttp(codigoHttp: number, mensaje: string){
+      Swal.fire({
+          title: 'Error http: ' + codigoHttp,
+          text: mensaje,
+          icon: 'error'
+      });
+  }
+
+  public static dialogErrorCampos(mensaje: string, campos: Array<string>){
+      let li: string = '';
+
+      campos.forEach(campo => {
+          li += `<li style="list-style-type: disc;">
+                      ${campo}
+                  </li>`;
+      });
+
+      let html: string = `
+          ${mensaje}
+          <div style="text-align: left;">
+              <br>
+              <ul>
+                  ${li}
+              </ul>
+          <div>
+      `;
+      
+      Swal.fire({
+          title: `<strong>Error</strong>`,
+          icon: 'error',
+          html: html
+      })
+  }
+
+  public static dialogImprimir(enlace: string){
+
+      let html: string = `
+          <p>
+              La solicitud de arrendamiento se ha creado correctamente:
+              <a href="${enlace}" target="_blank">
+                  Abrir documento
+              </a>
+          </p>
+      `;
+      
+      Swal.fire({
+          icon: 'success',
+          html: html
+      })
+  }
 
 }
