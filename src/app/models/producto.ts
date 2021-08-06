2d4061cba8eff445414e4bbc8817ee4cef84a86d
Daniel López <daniel315.a@gmail.com>
2021-08-05 21:54:47 -0500

    Se crea el modelo de productos

---
+++ src/app/models/producto.ts
@@ -0,0 +1,31 @@
+import { Empresa } from "./empresa";
+
+export class Producto{
+
+    constructor(
+        public id: number = 0,
+        public empresa: Empresa = new Empresa(),
+        public referencia: string = '',
+        public detalle: string = '',
+        public cantidadInterna: number = 0.0,
+        public cantidadDisponible: number = 0.0,
+        public precio: number = 0.0,
+        public habilitado: boolean = false
+    ){}
+
+    public inicializar(datos: any){
+        if(!!datos){
+            this.id = !!datos.id ? datos.id : this.id;
+            this.empresa = new Empresa();
+            this.referencia = !!datos.referencia ? datos.referencia : this.referencia;
+            this.detalle = !!datos.detalle ? datos.detalle : this.detalle;
+            this.cantidadInterna = !!datos.cantidad_interna ? datos.cantidad_interna : this.cantidadInterna;
+            this.cantidadDisponible = !!datos.cantidad_disponible ? datos.cantidad_disponible : this.cantidadDisponible;
+            this.precio = !!datos.precio ? datos.precio : this.precio;
+            this.habilitado = !!datos.habilitado ? datos.habilitado == 1 : this.habilitado;
+
+            this.empresa.inicializar(datos.empresa);
+        }
+    }
+
+}
