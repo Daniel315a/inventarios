f640d8e27728735bd004478a3a2a5f2bebea7d67
Daniel López <daniel315.a@gmail.com>
2021-08-07 23:00:49 -0500

    Se crean los métodos de crear y actualizar en el servicio de personas

--- src/app/services/persona.service.ts
+++ src/app/services/persona.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
+import { Persona } from "../models/persona";
 import { Utilidades } from "../models/utilidades";
 
 @Injectable()
@@ -31,6 +32,49 @@ export class PersonaService {
         set('id', id.toString());
 
         return this._http.get(this.url, {params:  parametros});
+    }
+
+    public crear(persona: Persona): Observable<any>{
+        const usuario = Utilidades.obtenerUsuario();
+        const parametros: HttpParams = new HttpParams().
+        set('token', usuario.token).
+        set('solicitud', 'crear').
+        set('municipio', persona.municipio.id.toString()).
+        set('tipo', persona.tipo.id.toString()).
+        set('empresa', persona.empresa.id.toString()).
+        set('tipo_documento', persona.tipoDocumento.id.toString()).
+        set('numero_documento', persona.numeroDocumento).
+        set('nombres', persona.nombres).
+        set('apellidos', persona.apellidos).
+        set('razon_social', persona.razonSocial).
+        set('direccion', persona.direccion).
+        set('telefono', persona.telefono).
+        set('email', persona.email).
+        set('habilitada', '1');
+
+        return this._http.post(this.url, parametros);
+    }
+
+    public actualizar(persona: Persona): Observable<any>{
+        const usuario = Utilidades.obtenerUsuario();
+        const parametros: HttpParams = new HttpParams().
+        set('token', usuario.token).
+        set('solicitud', 'actualizar').
+        set('id', persona.id.toString()).
+        set('municipio', persona.municipio.id.toString()).
+        set('tipo', persona.tipo.id.toString()).
+        set('empresa', persona.empresa.id.toString()).
+        set('tipo_documento', persona.tipoDocumento.id.toString()).
+        set('numero_documento', persona.numeroDocumento).
+        set('nombres', persona.nombres).
+        set('apellidos', persona.apellidos).
+        set('razon_social', persona.razonSocial).
+        set('direccion', persona.direccion).
+        set('telefono', persona.telefono).
+        set('email', persona.email).
+        set('habilitada', '1');
+
+        return this._http.post(this.url, parametros);
     }
 
 }
