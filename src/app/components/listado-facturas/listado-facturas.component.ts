ae26c3e1a82b201e052e5aa915889347257dff5a
Daniel López <daniel315.a@gmail.com>
2021-08-08 10:18:59 -0500

    Se crea la funcionalidad de consultar el listado de facturas

--- src/app/components/listado-facturas/listado-facturas.component.ts
+++ src/app/components/listado-facturas/listado-facturas.component.ts
@@ -1,9 +1,14 @@
 import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FacturaService } from 'src/app/services/factura.service';
 
 @Component({
   selector: 'app-listado-facturas',
   templateUrl: './listado-facturas.component.html',
   styleUrls: ['./listado-facturas.component.css'],
+  providers: [
+    FacturaService
+  ],
   host: {
     '(window:resize)': 'onResize($event)'
   },
@@ -33,9 +38,12 @@ export class ListadoFacturasComponent implements OnInit, AfterViewInit {
     acciones: 'Acciones'
   };
 
-  constructor() { }
+  constructor(
+    private _facturaService: FacturaService
+  ) { }
 
   ngOnInit(): void {
+    this.consultarListado();
   }
 
   ngAfterViewInit(): void {
@@ -55,6 +63,32 @@ export class ListadoFacturasComponent implements OnInit, AfterViewInit {
     if(altoDivTabla != this.altoDivTabla){
       this.altoDivTabla = altoDivTabla;
     }
+  }
+
+  public consultarListado(){
+    this.facturas = new Array<any>();
+
+    this._facturaService.consultarListado().subscribe(
+      result => {
+        if(result.resultado){
+          
+          result.datos.forEach(item => {
+            let factura: any = new Object();
+
+            factura.id = item.id;
+            factura.consecutivo = item.consecutivo;
+            factura.fecha = item.fecha;
+            factura.nombreCliente = item.nombre_cliente;
+            factura.valorTotal = item.valor_total;
+
+            this.facturas.push(factura);
+          });
+
+        }
+      }
+    );
+
+    console.log(this.facturas);
   }
 
 }
