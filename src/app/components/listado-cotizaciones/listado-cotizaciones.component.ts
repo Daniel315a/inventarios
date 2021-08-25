96926c17d7075ddb022c4a650ec161a8a09b82fb
Daniel López <daniel315.a@gmail.com>
2021-08-25 15:16:24 -0500

    Se maqueta el componente listado cotizaciones

--- src/app/components/listado-cotizaciones/listado-cotizaciones.component.ts
+++ src/app/components/listado-cotizaciones/listado-cotizaciones.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-listado-cotizaciones',
   templateUrl: './listado-cotizaciones.component.html',
-  styleUrls: ['./listado-cotizaciones.component.css']
+  styleUrls: ['./listado-cotizaciones.component.css'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  },
 })
-export class ListadoCotizacionesComponent implements OnInit {
+export class ListadoCotizacionesComponent implements OnInit, AfterViewInit {
+
+  /**
+   * Propiedades del diseño
+   */
+  public altoDivTabla: number = 0;
+  @ViewChild('divForm')
+  public divForm: ElementRef;
+  public labels = {
+    buscar: 'Buscar'
+  }
+
+  public botones = {
+    nuevo: 'Crear cotización'
+  }
+
+  public columnas = {
+    documentoCliente: 'N° Documento',
+    nombreCliente: 'Nombre / Razón social',
+    acciones: 'Acciones'
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.onResize();
+    });
+  }
+
+  public onResize(){
+    let altoDivForm:number = this.divForm.nativeElement.offsetHeight;
+    let altoDivTabla = window.innerHeight - altoDivForm - 110;
+    
+    if(altoDivTabla != this.altoDivTabla){
+      this.altoDivTabla = altoDivTabla;
+    }
   }
 
 }
