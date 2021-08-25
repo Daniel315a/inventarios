4c691919fe0a85a881629059c27cf6561445944c
Daniel López <daniel315.a@gmail.com>
2021-08-25 14:49:09 -0500

    Se crea la ruta cotizaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CotizacionesComponent } from "./components/cotizaciones/cotizaciones.component";
 import { FacturaComponent } from "./components/factura/factura.component";
+import { ListadoCotizacionesComponent } from "./components/listado-cotizaciones/listado-cotizaciones.component";
 import { ListadoFacturasComponent } from "./components/listado-facturas/listado-facturas.component";
 import { PersonaComponent } from "./components/persona/persona.component";
 import { PrestamoComponent } from "./components/prestamo/prestamo.component";
@@ -30,6 +31,10 @@ const routes: Routes = [
     },
     {
         path: 'cotizaciones',
+        component: ListadoCotizacionesComponent
+    },
+    {
+        path: 'cotizacion',
         component: CotizacionesComponent
     },
     {
